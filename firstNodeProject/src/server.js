a5f33fa6a8aafcd6739cbc4f94c1af95885a9a90
JRonca <jrfr21112000@gmail.com>
2025-03-31 07:24:41 -0300

    feat: crud in memoria

--- firstNodeProject/src/server.js
+++ firstNodeProject/src/server.js
@@ -1,15 +1,24 @@
 import http from 'node:http';
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   console.log(method, url);
 
   if (method === 'GET' && url === '/users') {
-    return res.end('List of users');
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Jhon Doe',
+      email: 'jhondoe@example.com'
+    })
     return res.end('Create users');
   }
 
