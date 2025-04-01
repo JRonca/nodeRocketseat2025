9579429ba170e8de2ff397b7e2e932cb026d9bb7
JRonca <jrfr21112000@gmail.com>
2025-04-01 08:22:03 -0300

    feat: persist DB and Random UUID

--- firstNodeProject/src/database.js
+++ firstNodeProject/src/database.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs';
+
+const databasePath = new URL('../db.json', import.meta.url);
+
 export class Database {
   #database = {};
+
+  constructor () {
+    fs.readFile(databasePath, 'utf-8', (err, data) => {
+      if (err) {
+        this.#persist();
+      } else {
+        this.#database = JSON.parse(data);
+      }
+    });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database), (err) => {
+      if (err) {
+        console.log('Error writing file:', err);
+      }
+    });
+  }
 
   select (table) {
     const data = this.#database[table] ?? [];
@@ -11,8 +33,10 @@ export class Database {
     if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data);
     } else {
-      this.#database[table] = data;
+      this.#database[table] = [data];
     }
+
+    this.#persist();
 
     return data;
   }
