29912bafddc0b437631f43400f847e0cded78919
JRonca <jrfr21112000@gmail.com>
2025-04-01 21:05:12 -0300

    feat: dinamic routes

---
+++ firstNodeProject/src/routes.js
@@ -0,0 +1,53 @@
+import { randomUUID } from 'node:crypto';
+import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const users = database.select('users');
+
+      return res.end(JSON.stringify(users));
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const { name, email } = req.body;
+      const user = ({
+        id: randomUUID(),
+        name,
+        email
+      })
+
+      database.insert('users', user);
+
+      return res
+        .writeHead(201)
+        .end('Create users');
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      const users = database.select('users');
+      const userIndex = users.findIndex(user => user.id === id);
+
+      if (userIndex < 0) {
+        return res.writeHead(404).end('User not found');
+      }
+
+      users.splice(userIndex, 1);
+
+      return res.writeHead(204).end();
+    }
+  }
+];
---
+++ firstNodeProject/src/utils/build-route-path.js
@@ -0,0 +1,13 @@
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g;
+
+  const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-_]+)');
+  console.log(pathWithParams);
+
+  const pathRegex = new RegExp(`^${pathWithParams}`);
+
+  console.log(pathRegex);
+
+  return pathRegex;
+
+}
