7080c5f595f4eb79b674864ec9e589776cf3d98b
JRonca <jrfr21112000@gmail.com>
2025-04-14 07:52:21 -0300

    feat: categorize tests

--- apiRest/test/example.spec.ts
+++ apiRest/test/example.spec.ts
@@ -1,22 +1,24 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('User can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'Test Transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  it('Should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Test Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
