073e3a24b3871fd6d16e5865f306f3f5e6400d5c
JRonca <jrfr21112000@gmail.com>
2025-04-16 08:14:25 -0300

    feat: env and eslint

--- Rocketseat/3-API-SOLID/src/server.ts
+++ Rocketseat/3-API-SOLID/src/server.ts
@@ -1,12 +1,16 @@
-import { app } from "./app";
+import { app } from './app';
+import { env } from './env';
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333
-}, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server is running at ${address}`);
-});
+app.listen(
+  {
+    host: '0.0.0.0',
+    port: env.PORT,
+  },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server is running at ${address}`);
+  },
+);
