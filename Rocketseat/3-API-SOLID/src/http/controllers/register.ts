e842119c4f202f1a255a1506ac487cfc3e7e4aa0
JRonca <jrfr21112000@gmail.com>
2025-04-19 15:38:55 -0300

    feat: error handling global

--- Rocketseat/3-API-SOLID/src/http/controllers/register.ts
+++ Rocketseat/3-API-SOLID/src/http/controllers/register.ts
@@ -22,13 +22,14 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       email,
       password,
     });
-  } catch (err) {
+  } catch (err: unknown) {
     if (err instanceof UserAlreadyExistsError) {
       return reply.status(409).send({
         message: err.message,
       });
     }
-    return reply.status(500).send();
+
+    throw err;
   }
 
   return reply.status(201).send();
