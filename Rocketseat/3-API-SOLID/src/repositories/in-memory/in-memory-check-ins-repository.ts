a3e8098db71c7cd83e1a09dc2b34e85cb750e5d8
JRonca <jrfr21112000@gmail.com>
2025-05-06 08:52:49 -0300

    feat: validate check in use case

--- Rocketseat/3-API-SOLID/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ Rocketseat/3-API-SOLID/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,16 @@ import dayjs from 'dayjs';
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async findById(id: string) {
+    const checkIn = this.items.find((checkIn) => checkIn.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date');
     const endOfTheDay = dayjs(date).endOf('date');
@@ -45,6 +55,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     };
 
     this.items.push(checkIn);
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const index = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (index >= 0) {
+      this.items[index] = checkIn;
+    }
 
     return checkIn;
   }
