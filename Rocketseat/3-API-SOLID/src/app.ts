d04f123ad2f09d6259377631011d57e6bdb61536
JRonca <jrfr21112000@gmail.com>
2025-05-20 08:30:44 -0300

    tests gyms e2e teste

--- Rocketseat/3-API-SOLID/src/app.ts
+++ Rocketseat/3-API-SOLID/src/app.ts
@@ -4,7 +4,7 @@ import { ZodError } from 'zod';
 import { env } from './env';
 import fastifyJwt from '@fastify/jwt';
 import { gymsRoutes } from './http/controllers/gyms/routes';
-import { checkInsRoutes } from './http/controllers/checkins/routes';
+import { checkInsRoutes } from './http/controllers/check-ins/routes';
 
 export const app = fastify();
 
