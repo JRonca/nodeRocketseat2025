0df6378431180a6b8dcf4ffde01ff73d168eea68
JRonca <jrfr21112000@gmail.com>
2025-05-23 08:33:08 -0300

    feat: refact node base

--- Rocketseat/3-API-SOLID/src/use-cases/errors/late-check-in-validation-error.ts
+++
@@ -1,5 +0,0 @@
-export class LateCheckInValidationError extends Error {
-  constructor() {
-    super('The check-in can only be validated until 20 minutes of creation.');
-  }
-}
--- Rocketseat/3-API-SOLID/src/use-cases/factories/make-create-gym-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository';
-import { CreateGymUseCase } from '../create-gym';
-
-export function makeCreateGymUseCase() {
-  const gymsRepository = new PrismaGymsRepository();
-  const createGymUseCase = new CreateGymUseCase(gymsRepository);
-
-  return createGymUseCase;
-}
